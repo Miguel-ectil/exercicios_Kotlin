40a85b5ecd4ffb6f968d150c23050a60f790a4d9
Miguel Ectil <87404714+Miguel-ectil@users.noreply.github.com>
2025-03-03 15:04:09 -0300

    Update Ex_30.kt
    [ADD] Adicionando o código do exercício 30

--- computed_conditions/Ex_30.kt
+++ computed_conditions/Ex_30.kt
@@ -1,2 +1,22 @@
 fun main() {
+    print("Digite o comprimento do primeiro segmento: \n")
+    val lado1 = 5
+
+    print("Digite o comprimento do segundo segmento: \n")
+    val lado2 = 5
+
+    print("Digite o comprimento do terceiro segmento: \n")
+    val lado3 = 2
+
+    if (lado1 + lado2 > lado3 && lado1 + lado3 > lado2 && lado2 + lado3 > lado1) {
+        if (lado1 == lado2 && lado2 == lado3) {
+            println("É um triângulo EQUILÁTERO.")
+        } else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+            println("É um triângulo ISÓSCELES.")
+        } else {
+            println("É um triângulo ESCALENO.")
+        }
+    } else {
+        println("Não é possível formar um triângulo com esses segmentos.")
+    }
 }
