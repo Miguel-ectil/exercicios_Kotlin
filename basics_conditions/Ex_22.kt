fbf714d833ecb806f9c9db2c6bd76670419b0e14
Miguel Ectil <87404714+Miguel-ectil@users.noreply.github.com>
2025-03-03 14:43:50 -0300

    Update Ex_22.kt
    [UPD] Corrigindo a condição do if else

--- basics_conditions/Ex_22.kt
+++ basics_conditions/Ex_22.kt
@@ -1,16 +1,18 @@
 import java.time.LocalDate
 
 fun main() {
-	val anoAtual = LocalDate.now().year
+  val anoAtual = LocalDate.now().year
     
-    print("Digite o seu ano de nascimento: \n")
-    val anoNasc = readln().toInt()
+  print("Digite o seu ano de nascimento: \n")
+  val anoNasc = readln().toInt()
     
-    val idade = anoAtual - anoNasc
+  val idade = anoAtual - anoNasc
     
-    if (idade <= 17) {
-        println("$idade\n você não pode se alistar no exercíto militar")
-    } else {
-        println("$idade\n você já pode se listar no exercíto militar")
-    }
+  if (idade < 18) {
+    val tempoRes = 18 - idade
+    println("você tem $idade anos, falta $tempoRes ano para o seu alistamento")
+  } else {
+    val tempoPassado = (anoAtual - anoNasc) - 18
+    println("Você tem $idade anos e já se passaram $tempoPassado anos do seu alistamento")
+  }
 }
