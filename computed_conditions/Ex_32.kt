8c64891537c71f4a69ba2e4dde4ff40bd9761e2d
Miguel Ectil <87404714+Miguel-ectil@users.noreply.github.com>
2025-03-03 15:20:57 -0300

    Create Ex_32.kt
    [ADD] Adicionando o exercício 32, utilizando o random no koltin

---
+++ computed_conditions/Ex_32.kt
@@ -0,0 +1,14 @@
+import kotlin.random.Random
+
+fun main() {
+  val numeroAleatorio = Random.nextInt(1, 5)
+  
+  println("Qual é o número sorteado? $numeroAleatorio\n")
+  val num = 2
+    
+  if (num == numeroAleatorio) {
+    println("Parabéns você acertou o número secreto $numeroAleatorio")
+  } else {
+    println("Errou! você não acertou o número secreto, tente novamente.")
+  }
+}
